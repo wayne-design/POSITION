9950397fda79bafb9edcfbaf35f5e8a25f8c329b
wayne-design <91874688+wayne-design@users.noreply.github.com>
2021-10-11 22:41:07 +0000

    Add files via upload

---
+++ page.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="c.css">
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <div class="foot">
+
+        </div>
+        <div class="kat">
+
+       
+        
+            <ul>
+               <p> <li class="r"><p>position</p></li>
+                <li id="acc"><a href="">Accueil</a></li>
+                <li id="av"><a href="">Event</a></li>
+                <li id="ap"><a href="">A propos</a></li>
+                <button name="btn" >s'inscrire</button></p>
+            </ul>
+        
+   
+</div>
+<div>
+    <span id="ya"></span>
+    <label class="anz">Anzoumana Andrea</label>
+    <label class="iles"><b>îles des caraîbes</b></label>
+    <main class="w">
+     <img src="../voila/voila/800032642.jpg" width="310px" height="252px">
+    </main>
+</div>
+<div>
+    <span id="yo"></span>
+    <label class="aff">Affran Chabel</label>
+    <label class="pain"><b>Monogaga</b></label>
+    <main class="j">
+        <img src="../voila/voila/800041308.jpg" width="310px" height="252px" id="co">
+    </main>
+</div>
+<div>
+    <span  id="and"></span>
+    <label class="bil">Bile Ezechiel</label>
+    <label class="gr"><b>Grand BereBi</b></label>
+    <main class="o">
+        <img src="../voila/voila/i3.jpg" width="310px" height="252px" id="chou">
+    </main>
+</div>
+</div>
+<div>
+
+</div>
+    </body>
+</html>
